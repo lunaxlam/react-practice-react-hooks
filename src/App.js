3aa503a8a19079d8e0f18bb9dae070dea481aa66
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-23 14:18:29 -0700

    Completed practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // The 2 golden rule of hooks are:
+  // 1. Hooks can only be created within a component
+  // 2. Hooks must be used at the top-level; cannot be nested or in a conditional statement
+  // Extra rule: Hooks must always be called in the exact same order
+
+  // The useState hook is simply a function used to update a state
+  // useState ALWAYS returns an array consisting of the default state and the function used to update the state
+  // const arr = useState(4) is never used; use destructuring instead
+
+  // useState consists of the default state and the function used to update the current state
+  const [count, setCount] = useState(4)
+
+  const [theme, setTheme] = useState('blue')
+
+
+  // Here is our update function that is triggered when the state changes
+  function decrementCount () {
+    // Always use an arrow function that passes the previous value
+
+    setCount(prevCount => (prevCount - 1))
+    setTheme('red')
+  }
+
+  function incrementCount() {
+    setCount(prevCount => (prevCount + 1))
+    setTheme('red')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <span>{theme}</span>
+    <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
